a1cf165fd765c711c182126951d5987c9a08fb52
Bill Flores <billflores@Bills-iMac.local>
2019-04-26 13:59:25 -0500

    add activity simple responsibility

---
+++ app/src/main/java/com/flores/solidandroid/simpleresponsibilityprinciple/SimpleResponsibilityPrincipleActivity.kt
@@ -0,0 +1,13 @@
+package com.flores.solidandroid.simpleresponsibilityprinciple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flores.solidandroid.R
+
+class SimpleResponsibilityPrincipleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple_responsibility_principle)
+    }
+}
