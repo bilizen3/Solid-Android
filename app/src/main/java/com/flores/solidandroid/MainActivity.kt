cb613d5533408398c6b3f0456d675cfcc3c11cff
Bill Flores <bilizen3@gmail.com>
2019-05-01 23:59:28 -0500

    add simple responsibility

--- app/src/main/java/com/flores/solidandroid/MainActivity.kt
+++ app/src/main/java/com/flores/solidandroid/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.flores.solidandroid.openclosedprinciple.OpenClosedPrincipleActivity
+import com.flores.solidandroid.simpleresponsibilityprinciple.SimpleResponsibilityPrincipleActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         btnOpenClosedPrinciple.setOnClickListener {
             startActivity(Intent(this, OpenClosedPrincipleActivity::class.java))
+        }
+        btnSimpleResponsiblidityPrinciple.setOnClickListener {
+            startActivity(Intent(this, SimpleResponsibilityPrincipleActivity::class.java))
         }
     }
 }
